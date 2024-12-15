a473dcea26556581171b8afcb345a74bcfaf128a
muneeb <saeedmuneeb665@gmail.com>
2024-12-15 23:27:00 +1100

    learning about booleans in python

---
+++ boolean.py
@@ -0,0 +1,11 @@
+#learning boolean values
+
+name= 42==42
+truy = not True
+fry = not not True
+pry = True or True 
+
+no = (4>3)and(4<3)
+yes = (4>3)or(4<3)
+print(name, truy, fry, pry, no, yes) 
+ 
